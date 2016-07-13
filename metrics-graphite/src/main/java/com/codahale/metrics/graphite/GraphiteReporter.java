786eb7f781be3d340cdc85b10c32611df6f28698
voyageth <voyageth@gmail.com>
2016-07-13 16:05:04 +0900

    - wrap try-catch on report method

--- metrics-graphite/src/main/java/com/codahale/metrics/graphite/GraphiteReporter.java
+++ metrics-graphite/src/main/java/com/codahale/metrics/graphite/GraphiteReporter.java
@@ -166,27 +166,47 @@ public class GraphiteReporter extends ScheduledReporter {
         // oh it'd be lovely to use Java 7 here
         try {
             if (!graphite.isConnected()) {
-    	          graphite.connect();
+                graphite.connect();
             }
 
             for (Map.Entry<String, Gauge> entry : gauges.entrySet()) {
-                reportGauge(entry.getKey(), entry.getValue(), timestamp);
+                try {
+                    reportGauge(entry.getKey(), entry.getValue(), timestamp);
+                } catch (Exception e) {
+                    LOGGER.warn("Unable to report to Graphite", graphite, e);
+                }
             }
 
             for (Map.Entry<String, Counter> entry : counters.entrySet()) {
-                reportCounter(entry.getKey(), entry.getValue(), timestamp);
+                try {
+                    reportCounter(entry.getKey(), entry.getValue(), timestamp);
+                } catch (Exception e) {
+                    LOGGER.warn("Unable to report to Graphite. key : " + entry.getKey(), graphite, e);
+                }
             }
 
             for (Map.Entry<String, Histogram> entry : histograms.entrySet()) {
-                reportHistogram(entry.getKey(), entry.getValue(), timestamp);
+                try {
+                    reportHistogram(entry.getKey(), entry.getValue(), timestamp);
+                } catch (Exception e) {
+                    LOGGER.warn("Unable to report to Graphite. key : " + entry.getKey(), graphite, e);
+                }
             }
 
             for (Map.Entry<String, Meter> entry : meters.entrySet()) {
-                reportMetered(entry.getKey(), entry.getValue(), timestamp);
+                try {
+                    reportMetered(entry.getKey(), entry.getValue(), timestamp);
+                } catch (Exception e) {
+                    LOGGER.warn("Unable to report to Graphite. key : " + entry.getKey(), graphite, e);
+                }
             }
 
             for (Map.Entry<String, Timer> entry : timers.entrySet()) {
-                reportTimer(entry.getKey(), entry.getValue(), timestamp);
+                try {
+                    reportTimer(entry.getKey(), entry.getValue(), timestamp);
+                } catch (Exception e) {
+                    LOGGER.warn("Unable to report to Graphite. key : " + entry.getKey(), graphite, e);
+                }
             }
 
             graphite.flush();
